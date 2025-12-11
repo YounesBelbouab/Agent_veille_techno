2313024c59839bfd29cae2650233a66b310d8da2
Belbouab <Belbouab.Younes@gmail.com>
2025-12-11 11:21:06 +0100

    ajout de l'agent de conversation

---
+++ conversation_agent.py
@@ -0,0 +1,57 @@
+from groq import Groq
+from dotenv import load_dotenv
+import os
+
+
+class ConversationAgent:
+    def __init__(self):
+        load_dotenv()
+        self.client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+        self.initiate_history()
+
+    @staticmethod
+    def read_file(file_path):
+        with open(file_path, "r", encoding="utf-8") as file:
+            return file.read()
+
+    def initiate_history(self):
+        self.history = [
+            {
+                "role": "system",
+                "content": ConversationAgent.read_file("./context.txt")
+            }]
+
+    def update_history(self, role, content):
+        self.history.append(
+            {
+                "role": role,
+                "content": content,
+            })
+
+    def ask_llm(self, user_interaction, model):
+        self.update_history(role="user", content=user_interaction)
+
+        response = self.client.chat.completions.create(
+            messages=self.history,
+            model=model
+        ).choices[0].message.content
+
+        self.update_history(role="assistant", content=response)
+
+        return response
+
+    def terminal_user_interface(self, model):
+        while True:
+            user_interaction = input("Vous : ")
+            if user_interaction.lower() == "exit":
+                break
+            elif user_interaction == "":
+                print("...")
+            else:
+                response = self.ask_llm(user_interaction=user_interaction, model=model)
+                print(f"Jarvis : {response}")
+
+
+if __name__ == "__main__":
+    conversation_agent = ConversationAgent()
+    conversation_agent.terminal_user_interface(model="llama-3.3-70b-versatile")
